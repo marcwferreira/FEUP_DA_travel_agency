651bf9691646e1dd106b69e52796efd15568b11c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-05-31 00:35:49 +0100

    documentação

--- codigo/Graph.h
+++ codigo/Graph.h
@@ -78,7 +78,7 @@ class Graph {
         void case1_a(int origin, int destiny);
 
         /**
-         * @brief Functions that calculates the path that allows for the largest group size and the least
+         * @brief Function that calculates the path that allows for the largest group size and the least
          * number of bus changes (prints both routes)
          * 
          * @param origin - an integer value representing the origin node
@@ -87,7 +87,8 @@ class Graph {
         void case1_b(int origin, int destiny);
 
         /**
-         * @brief Function that finds travel routes for a group,
+         * @brief Function that finds travel routes for a group, allowing division into subgroups with
+         * different paths
          * 
          * @param origin - an integer value representing the origin node
          * @param destiny - an integer value representing the destiny node
@@ -96,56 +97,87 @@ class Graph {
         void case2_a(int origin, int destiny, int groupSize);
 
          /**
-         * @brief 
+         * @brief Function that receives travel routes and the group size for it, then corrects the routes
+         * to accommodate for a different group size (if new group size is smaller or the same as before
+         * it returns the existing paths)
          * 
          * @param origin - an integer value representing the origin node
-         * @param destiny - an integer value representing the destiny 
-         * @param pathList - a list of integer lists representing the user's input
+         * @param destiny - an integer value representing the destiny node
+         * @param pathList - a list of integer lists representing the user's input paths
          * @param oldGroupSize - an integer value representing the desired group size
          * @param newGroupSize - an integer value representing the desired group size
          */
         void case2_b(int origin, int destiny, vector<vector<int>> pathList, int oldGroupSize, int newGroupSize);
 
         /**
-         * @brief 
+         * @brief Function that calculates the largest group size and travel routes between origin and destination,
+         * allowing group to divide into different paths
          * 
-         * @param origin 
-         * @param destiny
+         * @param origin - an integer value representing the origin node
+         * @param destiny - an integer value representing the destiny node
          */
         void case2_c(int origin, int destiny);
 
         /**
-         * @brief 
+         * @brief Function that calculates the minimum travel time for the arrival off the whole group to the end 
+         * destination (taking into account the different paths traveled)
          * 
-         * @param origin 
-         * @param destiny
-         * @param pathList
-         * @param oldGroupSize
-         * @param newGroupSize
-         * @param groupSize 
+         * @param origin - an integer value representing the origin node
+         * @param destiny - an integer value representing the destiny node
+         * @param pathList - a list of integer lists representing the user's input paths
          */
         void case2_d(int origin, int destiny, vector<vector<int>> pathList);
 
         /**
-         * @brief 
+         * @brief Function that calculates all the waiting times between bus changes that occurs in the give travel routes
+         * considering that the group
          * 
-         * @param origin 
-         * @param destiny
-         * @param pathList
-         * @param oldGroupSize
-         * @param newGroupSize
-         * @param groupSize 
+         * @param origin - an integer value representing the origin node
+         * @param destiny - an integer value representing the destiny node
+         * @param pathList - a list of integer lists representing the user's input paths
          */
         void case2_e(int origin, int destiny, vector<vector<int>> pathList);
-
+        
+        /**
+         * @brief 
+         * 
+         * @param origin - an integer value representing the origin node
+         * @param destiny - an integer value representing the destiny node
+         * @return int - representing the maximum capacity of discovered path
+         */
         int BFS(int origin, int destiny);
-
+        
+        /**
+         * @brief 
+         * 
+         * @param origin - an integer value representing the origin node
+         * @param destiny - an integer value representing the destiny node
+         * @param path - an integerx list that will be filled with a path
+         * @param capacity - maximum capacity of current path
+         * @return int - representing if an error occurs
+         */
         int pathBuild(int origin, int destiny, vector<int> &path, int capacity);
-
+        
+        /**
+         * @brief 
+         * 
+         * @param pathList 
+         * @param path 
+         * @param capacities 
+         * @param capacity 
+         * @param remainderSize 
+         * @return int - representing if an error occurs
+         */
         int verifyFoundPath(vector<vector<int>> &pathList, vector<int> &path, vector<int> &capacities, int &capacity, int remainderSize = INF);
-
+        
+        /**
+         * @brief
+         */
         void getEarliestStart();
-
+        
+        /**
+         * @brief 
+         */
         void getEarliestArrival();
 
     public:
