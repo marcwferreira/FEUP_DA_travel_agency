9ede1ac320bacaa62f10345965d685fd3fa5452a
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-06-03 03:15:47 +0100

    testing

--- AvaliaçãoEmpírica/run.sh
+++ AvaliaçãoEmpírica/run.sh
@@ -1,15 +1,39 @@
-for case in {1..2}
+echo "Case 1":
+for type in {1..2}
 do
-    MIN=1
-    MAX=$(($case == 1 ? 2 : 5))
-    for (( type=$MIN; type<=$MAX ; type++ ))
-    do
-        echo "Case $case - Type $type:"
-        for ((i=200; i<=4000; i+=200))  
-        do  
-            python3 graphGenerator.py "$i";
-            bash time.sh "$case" "$type" "$i"
-            rm -r "../input/graph$i.txt";
-        done 
-    done
+    echo "Type $type:"
+    for ((i=1000; i<=10000; i+=1000))  
+    do  
+        python3 graphGenerator.py "$i";
+        bash time.sh "1" "$type" "$i" "100"
+        rm -r "../input/graph$i.txt";
+    done 
+done
+
+echo "Case 2"
+for type in 1 3
+do
+    echo "Type $type"
+    for ((i=1000; i<=10000; i+=1000))  
+    do  
+        python3 graphGenerator.py "$i";
+        bash time.sh "2" "$type" "$i" "20"
+        rm -r "../input/graph$i.txt";
+    done 
+done
+
+echo "Type 2"
+for ((i=10; i<=100; i+=10))  
+do  
+    bash time.sh "2" "2" "10000" "$i" "input.txt"
+done 
+
+
+for type in 4 5
+do
+    echo "Type $type"
+    for ((i=5; i<=50; i+=5))  
+    do  
+        bash time.sh "2" "$type" "$i" "$i"
+    done 
 done
--- AvaliaçãoEmpírica/time.sh
+++ AvaliaçãoEmpírica/time.sh
@@ -2,7 +2,7 @@ TIMEFORMAT='%3R'
 time {
     cd ..
     cd build/
-    ./proj2 $1 $2 graph$3.txt 1 200
+    ./proj2 $1 $2 graph$3.txt 1 200 < $4
     cd ..
     cd AvaliaçãoEmpírica/
 } >> output.csv
